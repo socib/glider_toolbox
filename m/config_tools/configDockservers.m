306f451f19a58ff1fa1a37ebf812814e8c0b74c6
Joan Pau Beltran <joanpau.beltran@socib.cat>
2012-08-01 11:59:56 +0200

    Added dockserver configuration file.

---
+++ m/config_tools/configDockservers.m
@@ -0,0 +1,35 @@
+function dockservers = configDockservers()
+%CONFIGDOCKSERVERS  Configure dockserver parameters for glider data processing.
+%
+%  DOCKSERVERS = CONFIGDOCKSERVERS() should return a struct array with the
+%  access parameters for the dockservers containing Slocum glider data.
+%  Each of this dockservers is checked for new data in the processing chain.
+%  Returned DOCKSERVERS struct should have the following fields:
+%    URL: url of the dockserver (either fully qualified name or IP address as string).
+%    USER: user to access the dockserver (string).
+%    PASS: password of the dockserver (string).
+%
+%  Notes:
+%    Edit this file filling in the parameters of your dockservers.
+%    Once configured, this file will contain private data, so you should not
+%    make it public.
+%
+%  Examples:
+%    dockservers = configDockservers()
+%
+%  See also:
+%
+%  Author: Joan Pau Beltran
+%  Email: joanpau.beltran@socib.cat
+
+  error(nargchk(0, 0, nargin, 'struct'));
+
+  dockservers(1).url  = 'http://mydockserver01.myportal.mydomain';
+  dockservers(1).user = 'myself';
+  dockservers(1).pass = 'top_secret';
+
+  dockservers(2).url  = 'http://mydockserver02.myportal.mydomain';
+  dockservers(2).user = 'myself';
+  dockservers(2).pass = 'top_secret';
+
+end
