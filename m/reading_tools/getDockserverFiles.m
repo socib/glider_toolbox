a774d449b5065b10a609d82a8bf7048b92843331
Joan Pau Beltran <joanpau.beltran@socib.cat>
2012-08-07 12:00:21 +0200

    Refactored dockserver fetching function.

--- m/reading_tools/getDockserverFiles.m
+++ m/reading_tools/getDockserverFiles.m
@@ -1,240 +1,231 @@
-function [fetchedSbdList, fetchedLogList] = getDockserverFiles(gliderName, gliderParams)
-%GETDOCKSERVERFILES - Gets binary data files and logs from dockserver through FTP
-% Retrieves .?bd files and surface dialog files from the dockserver URL
-% contained in the glider parameters structure.
+function [bin_files, log_files] = getDockserverFiles(dockserver, glider_name, local_bin_dir, local_log_dir, varargin)
+%GETDOCKSERVERFILES  Get binary data files and logs from dockserver through FTP.
 %
-% Syntax: [fetchedSbdList, fetchedLogList] = getDockserverFiles(gliderName, gliderParams)
+%  GETDOCKSERVERFILES(DOCKSERVER, GLIDER_NAME, LOCAL_BIN_DIR, LOCAL_LOG_DIR) and
+%  GETDOCKSERVERFILES(..., 'option1', 'value1', 'option2', 'value2' ...)
+%  retrieve  new .[smdtne]bd files and surface dialog files of the glider named
+%  GLIDER_NAME from DOCKSERVER to  LOCAL_BIN_DIR and LOCAL_LOG_DIR respectively,
+%  returning string cell arrays  with the list of downloaded files.
+%  Files already existing in the local directories are considered older than the
+%  ones in the dockserver it they are smaller.
 %
-% Inputs:
-%    gliderName - name of the glider from which to retrieve the data
-%    gliderParams - structure with information on where to get files from
+%  DOCKSERVER is a struct with the fields needed by the function FTP:
+%    HOST: url as either fully qualified name or IP (string).
+%    USER: user to access the dockserver (string).
+%    PASS: password of the dockserver (string).
 %
-% Outputs:
-%    fetchedSbdList - list of binary data files
-%    fetchedLogList - list of log files
+%  The list of files to download may be restricted with the following options:
+%    'start': do not not download files previous to the given date.
+%        Its value may be any valid input to the function DATENUM.
+%    'end': do not not download files previous to the given date.
+%        Its value may be any valid input to the function DATENUM.
+%    'bin_name': download only binary files matching given pattern.
+%        Its value may be any valid regular expression string or false.
+%        Set it to the empty string or false to disable downloading.
+%    'log_name': download only log files matching given pattern.
+%        Its value may be any valid regular expression string.
+%        Set it to the empty string or false to disable downloading.
 %
-% Example:
-%    [fetchedSbdList, fetchedLogList] = getDockserverFiles(gliderName, gliderParams)
+%  Notes:
+%    Date filtering is done based on the mission date from the file names, not
+%    on the file attributes. Hence it relies on remote file names having the
+%    conventional Slocum file name format.
+%    For binary files it is:
+%      ru07-2011-347-4-0.sbd
+%    where
+%      ru07: glider name.
+%      2011: year in which the mission was started.
+%      347: zero-based day of the year on which the mission was started.
+%      4: zero-based mission number for the day the mission was started.
+%      0: zero-based segment number of the current mission number.
+%    For log files it is:
+%      icoast00_modem_20120510T091438.log
+%    where
+%      icoast00: glider name.
+%      modem: transmission method ('modem' or 'network').
+%      20120510T091438: ISO 8601 UTC timestamp.
 %
-% Other m-files required: none
-% Subfunctions: ftpConnect, ftpReconnect, downloadFiles
-% MAT-files required: none
+%    This function is based on the previous work by Tomeu Garau. He is the true
+%    glider man.
 %
-% See also: PARSECONFIGFILE, FTP
+%  Examples:
+%   dockservers = configDockservers()
+%   dockserver = dockservers(1)
+%   glider_name = my_glider
+%   bin_dir = '~/my_glider/binary'
+%   log_dir = '~/my_glider/log'
+%   % Get all binary and log files.
+%   [bin_files, log_files] = getDockserverFiles(dockserver, glider_name, ...
+%                                               bin_dir, log_dir)
+%   % Get only small files and no logs from last week:
+%   [bin_files, log_files] = getDockserverFiles(dockserver, glider_name, ...
+%                                               bin_dir, log_dir, ...
+%                                               'bin_name', '^*.[st]bd$', ...
+%                                               'log_name', '',
+%                                               'start', now()-7, 'end', now())
 %
-% Authors: John Kerfoot & Bartolome Garau
-% Work address:
-% Author e-mail: kerfoot@imcs.rutgers.edu & tgarau@socib.es
-% Website: http://rucool.marine.rutgers.edu & http://www.socib.es
-% Creation: 18-Feb-2011
+%  See also:
+%    FTP
+%    DATENUM
 %
+%  Author: Joan Pau Beltran
+%  Email: joanpau.beltran@socib.cat
 
-%% Initialize output as empty files list
-    fetchedSbdList = {};
-    fetchedLogList = {};
+  %% Argument handling.
+  error(nargchk(3, 11, nargin, 'struct'));
 
-%% Dockserver (remote) folder definition and parameters checking
-    dockserverDataBaseDir = ['/var/opt/gmc/gliders/', lower(gliderName)];
-    % Old dockservers used this other path:
-    %dockserverDataBaseDir = ['/home/dockserver/gliders/', lower(gliderName)];
-
-    if isempty(gliderParams)
-        disp(['Empty glider parameters data structure provided to ', mfilename]);
-        return;
+  date_filtering = false;
+  start_date = -Inf;
+  end_date = Inf;
+  bin_name = '^*.[smdtne]bd$';
+  log_name = '^*.log$';
+  for i=1:2:numel(varargin)
+    opt = varargin{i};
+    val = varargin{i+1};
+    switch opt
+      case 'start'
+        start_date = val;
+        date_filtering = true;
+      case 'end'
+        end_date = val;
+        date_filtering = true;
+      case 'bin_name'
+        bin_name = val;
+      case 'log_name'
+        log_name = val;
     end
+  end
 
-    % Set the glider data root directory
-    if ~isfield(gliderParams, 'data_root')
-        disp('Missing DATA_ROOT config parameter!');
-        return;
-    end
-    localDataBaseDir = gliderParams.data_root;
 
-    if ~(isfield(gliderParams, 'DOCKSERVER_URL') && ~isempty(gliderParams.DOCKSERVER_URL))
-        disp(['Could not find dockserver URL in glider parameters data structure provided to ', mfilename]);
-        return;
-    end
+  %% Dockserver (remote) directory definition.
+  % Consider pass these paths as (maybe optional) input arguments.
+  % Old dockservers used this other base path:
+  %remote_base_dir = '/home/dockserver/gliders';
+  remote_base_dir = '/var/opt/gmc/gliders';
+  remote_bin_dir = [remote_base_dir '/' lower(glider_name) '/' 'from-glider'];
+  remote_log_dir = [remote_base_dir '/' lower(glider_name) '/' 'logs'];
 
-%% Open ftp connection
 
-    ftpStruct.dockURL = gliderParams.DOCKSERVER_URL;
-    ftpStruct.ftpUser = gliderParams.DOCKSERVER_USER;
-    ftpStruct.ftpPass = gliderParams.DOCKSERVER_PASS;
-    disp(['Connecting to ' ftpStruct.dockURL ' ...']);
+  %% Name date format.
+  bin_date_scan = ...
+    @(s)( cellfun(@str2double, regexp(s, ...
+                                      '^.*-(\d{4})-(\d{3})-\d+-\d+\.[smdtne]bd$', ...
+                                      'tokens','once'))*[1 0 0; 0 0 1] + [0 0 1]);
+  log_date_scan = ...
+    @(s)( cellfun(@str2double, regexp(s, ...
+                                      '^.*_.*_(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})\.log$', ...
+                                      'tokens','once')) );
+
+
+  %% Date filtering parameters.
+  if date_filtering
+    bin_date_filtering_params = {start_date, end_date, bin_date_scan};
+    log_date_filtering_params = {start_date, end_date, log_date_scan};
+  else
+    bin_date_filtering_params = {};
+    log_date_filtering_params = {};
+  end
+
+
+  %% Open ftp connection.
+  disp(['Connecting to host ' dockserver.host '...']);
+  ftp_handle = ftp(dockserver.host, dockserver.user, dockserver.pass);
+
+
+  %% Binary data file downloading.
+  disp('Downloading binary data files...');
+  bin_files = {};
+  if bin_name
     try
-        ftpStruct = ftpConnect(ftpStruct);
-    catch ME
-        disp('Could not connect to dockserver');
-        disp(getReport(ME, 'extended'));
-        return;
-    end;
+     bin_files = fetchNewAndUpdatedFiles(ftp_handle, remote_bin_dir, ...
+                                         local_bin_dir, bin_name, ...
+                                         bin_date_filtering_params{:});
+    catch exception
+      warning('glider_toolbox:reading_tools:BinDownloadingError', ...
+              'Error downloading binary data files: %s', exception.message);
+    end
+    disp([num2str(numel(bin_files)) ' new/updated binary data files fetched.']);
+  end
 
-%% Starting date management
-    if ~isfield(gliderParams, 'start_time')
-        disp('No START_DATE parameter found in config file.');
-        startingDate = datenum([1970 1 1], 'yyyy-mm-dd');
-    else
-        startingDate = gliderParams.start_time;
-    end;
-    if ~isfield(gliderParams, 'end_time')
-        disp('No END_DATE parameter found in config file.');
-        endingDate = now;
-    else
-        endingDate = gliderParams.end_time;
-    end;
 
-    disp(['Downloading files between ', datestr(startingDate), ' and ', datestr(endingDate)]);
+  %% Surface log file downloading.
+  disp('Downloading surface log files...');
+  log_files = {};
+  if log_name
+    try
+     log_files = fetchNewAndUpdatedFiles(ftp_handle, remote_log_dir, ...
+                                         local_log_dir, log_name, ...
+                                         log_date_filtering_params{:});
+    catch exception
+      warning('glider_toolbox:reading_tools:LogDownloadingError', ...
+              'Error downloading surface log files: %s', exception.message);
+    end
+    disp([num2str(numel(log_files)) ' new/updated surface log files fetched.']);
+  end
 
-%% Binary data files downloading
+  %% Close ftp connection.
+  close(ftp_handle);
+  disp(['Closed connection to host ' dockserver.host '.']);
 
-    remoteSbdDir = [dockserverDataBaseDir, '/from-glider'];
-    localSbdDir = fullfile(localDataBaseDir, 'binary');
-    if ~exist(localSbdDir, 'dir')
-        [success, errorMsg] = mkdir(localSbdDir);
-        if isequal(success, 0)
-            disp(errorMsg);
-            return;
-        end;
-    end;
+end
 
-    fetchedSbdList = downloadFiles(ftpStruct, gliderName, 'binary', ...
-        remoteSbdDir, localSbdDir, ...
-        startingDate, endingDate);
-    disp([num2str(length(fetchedSbdList), '%0.0f') ' new/updated binary data files fetched.']);
 
-%% Surface log files downloading
-    remoteLogDir = [dockserverDataBaseDir, '/logs'];
-    localLogDir = fullfile(localDataBaseDir, 'logs');
-    if ~exist(localLogDir, 'dir')
-        [success, errorMsg] = mkdir(localLogDir);
-        if isequal(success, 0)
-            disp(errorMsg);
-            return;
-        end;
-    end;
-
-    fetchedLogList = downloadFiles(ftpStruct, gliderName, 'logfile', ...
-        remoteLogDir, localLogDir, ...
-        startingDate, endingDate);
-    disp([num2str(length(fetchedLogList), '%0.0f') ' new/updated surface log files fetched.']);
-
-%% Close the ftp connection
-    close(ftpStruct.ftpHandle);
+function files = fetchNewAndUpdatedFiles(ftp_handle, remote_dir, local_dir, name, start_date, end_date, date_scan)
+%FETCHNEWANDUPDATEDFILES Fetch only new or updated files from remote ftp directory.
+%
+%  FILES = FETCHNEWANDUPDATEDFILES(FTP_HANDLE, REMOTE_DIR, LOCAL_DIR, NAME)
+%  Download all files with names matching the pattern NAME from remote directory
+%  REMOTE_DIR  to local directory LOCAL_DIR using the valid ftp connection
+%  FTP_HANDLE. It returns the list of donloaded files.
+%
+%  FILES = FETCHNEWANDUPDATEDFILES(..., START_DATE, END_DATE, DATE_SCAN)
+%  filters the files to download to those ones whose date obtained from name
+%  scanning according to DATE_SCAN falls between START_DATE and END_DATE.
+  files = {};
+  % Enable date filtering if optional argument has be passed.
+  filter_date = (nargin==7);
+  % List remote files.
+  remote_files = dir(ftp_handle, remote_dir);
+  % Check the list of remote files.
+  if isempty(remote_files)
+    disp(['Non existing or empty remote directory ' remote_dir '.']);
     return;
-
-%% Sub-function downloadFiles
-    function fetchedFilesList = downloadFiles(ftpStruct, vehicleName, fileType, remoteDir, localDir, ...
-        startingDate, endingDate)
-
-        % Initialize output
-        fetchedFilesList = {};
-
-        if strcmp(fileType, 'binary')
-            filePattern = [lower(vehicleName) '*.*bd'];
-            fileDatePattern = [lower(vehicleName) '-%d-%d-%d-%d.'];
-        elseif strcmp(fileType, 'logfile')
-            filePattern = [lower(vehicleName) '*.log'];
-            fileDatePattern = [lower(vehicleName) '_%*[a-zA-Z0-9]_%04d%02d%02dT%02d%02d%02d.log'];
-        else
-            disp('Unknown file type to download');
-            return;
-        end;
-
-        % Get list of remote files
-        try
-            cd(ftpStruct.ftpHandle, remoteDir);
-            remoteFiles = dir(ftpStruct.ftpHandle, filePattern);
-            if isempty(remoteFiles)
-                disp(['No files on dockserver following pattern ', filePattern]);
-                return;
-            else
-                remoteNames = {remoteFiles(:).name}';
-                filesTimestamp = nan(length(remoteNames), 1);
-                for idx = 1:length(remoteNames)
-                    fnContent = sscanf(remoteNames{idx}, fileDatePattern);
-                    if strcmp(fileType, 'binary')
-                        filesTimestamp(idx) = datenum([fnContent(1), 1, fnContent(2)]) + 1;
-                    elseif strcmp(fileType, 'logfile')
-                        filesTimestamp(idx) = datenum(fnContent(:)');
-                    else
-                        disp('Unknown file type to download');
-                        return;
-                    end;
-                end;
-
-                areInPeriodIdx = (filesTimestamp >= floor(startingDate) & filesTimestamp <= endingDate);
-                remoteFiles = remoteFiles(areInPeriodIdx);
-                remoteFilenames = {remoteFiles.name}';
-            end;
-        catch MatExcep
-            disp('Could not get remote file list');
-            keyboard;
-            disp(getReport(MatExcep, 'extended'));
-            return;
-        end;
-
-        % Get list of local files
-        localFiles = dir(fullfile(localDir, filePattern));
-        localFilenames = {localFiles.name}';
-
-        % Loop through the list of remote (dockserver) files:
-        % 1 - if the file is not found locally, fetch it.
-        % 2 - if the file is found locally, re-fetch it only if
-        % it's size has increased.
-        for fileIdx = 1:length(remoteFilenames)
-            % If there is no local copy
-            if isempty(strmatch(remoteFilenames{fileIdx}, localFilenames))
-                disp(['Downloading new file: ', remoteFilenames{fileIdx}]);
-                [ftpStruct, fetchedFileName] = fetchOneFile(ftpStruct, remoteFilenames{fileIdx}, localDir);
-                if ~isempty(fetchedFileName)
-                    fetchedFilesList{end+1} = fetchedFileName{:};
-                end;
-
-            % Get remote file if is larger than local one
-            else
-                remoteSize = remoteFiles(fileIdx).bytes;
-                localFile = dir(fullfile(localDir, remoteFilenames{fileIdx}));
-                if ~isempty(localFile)
-                    localSize = localFile.bytes;
-                    if remoteSize > localSize
-                        disp(['Updating file: ', remoteFilenames{fileIdx}]);
-                        [ftpStruct, fetchedFileName] = fetchOneFile(ftpStruct, remoteFilenames{fileIdx}, localDir);
-                        if ~isempty(fetchedFileName)
-                            fetchedFilesList{end+1} = fetchedFileName{:};
-                        end
-                    end; % if remoteSize > localSize
-                end; % if ~isempty(localFile)
-            end; % if isempty(strmatch(remoteFilenames{fileIdx}, localFilenames))
-        end; % for fileIdx = 1:length(remoteFilenames)
+  end
+  % Go to remote directory (it should be save here).
+  cd(ftp_handle, remote_dir);
+  % Select files mathcing name pattern and date.
+  remote_match = (1 == cellfun('length',regexp({remote_files.name}, name)));
+  remote_files = remote_files(remote_match);
+  if filter_date
+    remote_dates = cellfun(@(s) datenum(date_scan(s)), {remote_files.name});
+    remote_indate = datenum(start_date) < remote_dates & remote_dates < datenum(end_date);
+    remote_files = remote_files(remote_indate);
+  end
+  if isempty(remote_files)
+    disp(['No files satisfying criteria in remote directory ' remote_dir '.']);
+    return;
+  end
+  % Check for files already existing in the local directory.
+  if exist(local_dir, 'dir')
+    % Select only new files or files whose size is bigger in the dockserver.
+    local_files = dir(local_dir);
+    [existing, local_idx] = ismember({remote_files.name}, {local_files.name});
+    updated = [remote_files(existing).bytes] > [local_files(local_idx(existing)).bytes];
+    to_download = ~existing;
+    to_download(existing) = updated;
+    remote_files = remote_files(to_download);
+  else
+    % Create local directory because mget does not do it.
+    [success, error_msg] = mkdir(local_dir);
+    if ~success
+      error('glider_toolbox:reading_tools:LocalDirectoryError', error_msg);
     end
-
-    function [ftpStruct, fetchedFileName] = fetchOneFile(ftpStruct, remoteFileName, localDir)
-        try
-            fetchedFileName = mget(ftpStruct.ftpHandle, remoteFileName, localDir);
-        catch MatExcep
-            disp('Fetch Error');
-            disp(getReport(MatExcep.stack, 'extended'));
-            try
-                ftpStruct = ftpReconnect(ftpStruct);
-            catch MatExcep2
-                disp('Reconnection failed too');
-                disp(getReport(MatExcep2.stack, 'extended'));
-            end;
-        end;
-    end
-
-%% Sub-function ftp handling
-    function newFtpStruct = ftpConnect(ftpStruct)
-        newFtpStruct = ftpStruct;
-        newFtpStruct.ftpHandle = ...
-            ftp(ftpStruct.dockURL, ftpStruct.ftpUser, ftpStruct.ftpPass);
-    end
-
-    function newFtpStruct = ftpReconnect(ftpStruct)
-        newFtpStruct = ftpStruct;
-        close(ftpStruct.ftpHandle);
-        newFtpStruct.ftpHandle = ...
-            ftp(ftpStruct.dockURL, ftpStruct.ftpUser, ftpStruct.ftpPass);
-    end
-
+  end
+  if isempty(remote_files)
+    disp(['No new/updated files in remote directory ' remote_dir '.']);
+    return;
+  end
+  % Download the files.
+  files = cellfun(@(f) mget(ftp_handle, f, local_dir), {remote_files.name}, ...
+                  'UniformOutput', false);
 end
