05e2becbc7e872fbc663aaf7dd64176596e3d1ac
Joan Pau Beltran <joanpau.beltran@socib.cat>
2012-09-11 09:56:13 +0200

    Updated main glider data processing script to adapt it to last changes.

--- m/main_glider_data_processing_nrt.m
+++ m/main_glider_data_processing_nrt.m
@@ -4,7 +4,7 @@
 %    - Check for active deployments from deployment information source.
 %    - Download new or updated deployment raw data files.
 %    - Convert downloaded files to human readable format if needed.
-%    - Preprocess raw data applying simple unit conversions data without
+%    - Preprocess raw data applying simple unit conversions data without 
 %      modifying it:
 %      1. NMEA latitude and longitude to decimal degrees.
 %    - Produce standarized product version of raw data.
@@ -25,18 +25,21 @@
 %  Email: joanpau.beltran@socib.cat
 
 
-%% Set the path to the data: edit for mission processing
-% Gliders configuration file directory
-glider_toolbox_dir = fileparts(mfilename('fullpath'));
+%% Configure toolbox and configuration file path.
+glider_toolbox_dir = configGliderToolboxPath();
+% Glider configuration file directory
 config_dir = fullfile(glider_toolbox_dir, 'config');
 desired_extensions = {'sbd', 'tbd'};
-glider_sub_dir_tree = {...
-  'binary',   ...
-  'ascii',  ...
-  'logs',   ...
-  'figures', ...
-  'netcdf'};
 
+
+%% Configure deployment data paths.
+config.local_paths = configLocalPathsRT();
+
+%% Configure NetCDF output.
+config.output_nc_l0 = configOutputNetCDFL0();
+
+
+%% Set the path to the data: edit for mission processing.
 %output_dirs.nc_base_path     = '/data/current/opendap/observational/auv/glider/';
 output_dirs.nc_base_path      = '/home/jbeltran/public_html/observational/auv/glider/';
 output_dirs.image_base_local_path = '/home/jbeltran/public_html/glider';
@@ -56,8 +59,8 @@ config.db_access = configDBAccess();
 [config.db_query, config.db_fields] = configDBDeploymentInfoQuery();
 
 
-%% Configure local data base path.
-config.local_base_path = '/home/jbeltran/public_html/glider/data_rt';
+%% Configure dockserver glider data source.
+config.dockservers = configDockservers();
 
 
 %% Get list of active deployments from database.
@@ -65,113 +68,107 @@ disp('Querying information of glider deployments...');
 active_deployments = getDBDeploymentInfo(config.db_access, ...
                                          config.db_query, ...
                                          config.db_fields);
-
-
-%% Process active deployments
+% active_deployments = active_deployments([active_deployments.deployment_id] == 94);
 if isempty(active_deployments)
   disp('No glider deployments available.');
   return
 end
 
-active_deployments = active_deployments(strcmp({active_deployments.glider_name},'ideep00'));
-
-for deployment_idx = 1:length(active_deployments)
+                                       
+%% Process active deployments
+for deployment_idx = 1:numel(active_deployments)
   %% Deployment complete configuration.
-  current_deployment = active_deployments(deployment_idx);
-  config_filename = fullfile(config_dir, [current_deployment.glider_name, '.cfg']);
-  if ~exist(config_filename, 'file')
-    disp(['Could not find ', config_filename]);
-    continue;
-  end;
+  deployment = active_deployments(deployment_idx);
 
-  % Add glider configuration parameters.
-  params = parseConfigFile(config_filename);
-  if isempty(params)
-    disp('Empty parameters data structure!');
-    continue;
-  end;
-  fields = fieldnames(params);
-  for field_idx = 1:length(fields)
-    current_field = fields{field_idx};
-    current_deployment.(current_field) = params.(current_field);
-  end;
-
+  %{
   % Fix possibly missing deployment end time.
-  if isempty(current_deployment.end_time)
-    current_deployment.end_time = now();
+  if isempty(deployment.end_time)
+    deployment.end_time = now();
   end
-
-
+  %}
+  
   % Set the glider data root directory
   %{
-  if ~isfield(current_deployment, 'data_root')
+  if ~isfield(deployment, 'data_root')
     disp('Missing DATA_ROOT config parameter. Setting default value...');
-    current_deployment.data_root = fullfile('/home/glider/glider_deployments/', glider_name, current_deployment.mission_name);
+    deployment.data_root = fullfile('/home/glider/glider_deployments/', glider_name, deployment.mission_name);
   end;
   %}
-  current_deployment.data_root = fullfile(config.local_base_path, ...
-                                          current_deployment.glider_name, ...
-                                          datestr(current_deployment.start_time,'yyyymmdd'));
-
-
-  %% Generate glider directory if it does not exist.
-  [success, error_msg] = mkdir(current_deployment.data_root);
-  if isequal(success,0)
-    disp(error_msg);
-    return;
-  end;
-
-  % Create child directories contained in GLIDERSUBDIRTREE
-  for dir_idx = 1:length(glider_sub_dir_tree)
-    [success, error_msg] = mkdir(fullfile(current_deployment.data_root, glider_sub_dir_tree{dir_idx}));
-    if isequal(success, 0)
-      disp(error_msg);
-      continue;
-    end;
-  end;
-
-  % Define directories
-  binary_dir = fullfile(current_deployment.data_root, 'binary');
-  ascii_dir  = fullfile(current_deployment.data_root, 'ascii');
-  image_dir  = fullfile(current_deployment.data_root, 'figures');
-
-
+  
+                                
+  %% Deployment field shortcut variables.
+  glider_name = deployment.glider_name;
+  binary_dir = strfglider(config.local_paths.binary_path, deployment);
+  log_dir = strfglider(config.local_paths.log_path, deployment);
+  ascii_dir = strfglider(config.local_paths.ascii_path, deployment);
+  image_dir = strfglider(config.local_paths.figure_path, deployment);
+%  netcdf_dir = strfglider(config.local_paths.netcdf_path, deployment);
+  ncl0_fullfile = strfglider(config.local_paths.netcdf_l0, deployment);
+  deployment_start = deployment.deployment_start;
+  deployment_end = deployment.deployment_end;
+  
+  
   %% Download glider files from station(s).
-  [new_sbds, new_logs] = getDockserverFiles(current_deployment.glider_name, ...
-                                            current_deployment);
+  % Check for new or updated deployment files in every dockserver and fetch them.
+  % Subsequent flattening would not be needed if uniform output worked properly.
+  % Deployment end time may be undefined.
+  [new_xbds, new_logs] = ...
+    arrayfun(@(d) getDockserverFiles(d, glider_name, binary_dir, log_dir, ...
+                                     'start', deployment_start, 'end', deployment_end), ...
+             config.dockservers(:)', 'UniformOutput', false);
+  new_xbds = [new_xbds{:}];
+  new_logs = [new_logs{:}];
 
-  %% Convert binary glider files.
+                                        
+  %% Convert binary glider files to human readable format.
   % 1 - convert binary data files to ascii
   % *Note: the parameter 'e' for extension needs to be specified
   % in order to merge files with new Science Data Logging feature
-  if isdir(ascii_dir) && isdir(binary_dir)
-    downloaded_loaders = convertSlocumBinaries([],...
-      's', binary_dir, ...
-      'd', ascii_dir, ...
-      'e', desired_extensions, ...
-      'f', fullfile(config_dir, 'sensorfilter.txt'), ...
-      'c', binary_dir);
-    disp([num2str(length(downloaded_loaders), '%0.0f'), ' files converted.']);
+  if ~isdir(ascii_dir)
+    [success, error_msg] = mkdir(ascii_dir);
+    if ~success
+      disp(['Error creating directory for ascii files ' ascii_dir ':']);
+      disp(error_msg);
+      disp('Deployment processing aborted.');
+      continue
+    end
   end
+%     new_dbas = cellfun(@(f) fullfile(ascii_dir,f), ...
+%                        regexprep(new_xbds,'\.([smdtne]bd)$', '_$1\.dba'));
+%     new_dbas = xbd2ascii(new_xbds, new_dbas, config.slocum_programs.dbd2ascii);
+  downloaded_loaders = convertSlocumBinaries([],...
+    's', binary_dir, ...
+    'd', ascii_dir, ...
+    'e', desired_extensions, ...
+    'f', fullfile(config_dir, 'sensorfilter.txt'), ...
+    'c', binary_dir);
+  disp([num2str(length(downloaded_loaders), '%0.0f'), ' files converted.']);
 
-
+    
   %% Load raw glider data (if there is something new).
   if isempty(downloaded_loaders)
-    disp('No new deployment data to process, omitting...');
+    disp('No new deployment data to process, omitting this deployment...');
     continue
   end
-  period = [current_deployment.start_time current_deployment.end_time];
-
-  raw_data = loadTransectData(ascii_dir, period);
-%{
-  % Generate L0 (Raw) netcdf file and store it
+  raw_data = loadTransectData(ascii_dir, [deployment_start deployment_end]);
+  
+  %% Generate L0 (raw) NetCDF file.
+  % Take default global attributes from configuration but overwrite the ones
+  % present in deployment struct.
+  raw_data_aux = struct();
+  for f = setdiff(fieldnames(raw_data),{'data' 'source'})
+    raw_data_aux.(f{:}) = raw_data.data(:,raw_data.(f{:}));
+  end
+  raw_meta = config.output_nc_l0.var_meta;
+  raw_dims = config.output_nc_l0.dim_names;
+  raw_atts = config.output_nc_l0.global_atts; 
   try
-    generateNcProduct(current_deployment, output_dirs, glider_name, 'L0', raw_data);
-  catch ME
-    disp('could not generate glider raw netcdf file');
-    disp(getReport(ME, 'extended'));
+    generateOutputNetCDFL0(ncl0_fullfile, deployment, ...
+                           raw_data_aux, raw_meta, raw_dims, raw_atts);
+  catch exception
+    disp(['Error creating L0 (raw data) NetCDF file ' ncl0_fullfile ': ']);
+    disp(getReport(exception));
   end;
-%}
 
 
   %% Process raw glider data to get clean trajectory data.
@@ -185,39 +182,38 @@ for deployment_idx = 1:length(active_deployments)
   end
 
   if isempty(processed_data)
-    disp('processed data is empty. Skipping storage, gridding and plotting.');
-    continue;
-  end;
-
-
-  %% Process glider trajectory data to vertically gridded data.
-  gridded_data = gridGliderData(processed_data);
+    disp('Processed data is empty. Skipping storage, gridding and plotting.');
+    continue
+  end
 %{
   % Generate L1 (Processed) netcdf file and store it
   try
     % Store results in mat file
-    proc_filename = [glider_name, '_L1_', datestr(current_deployment.start_date, 'yyyy-mm-dd')];
-    processed_data_filename = fullfile(current_deployment.data_root, 'matfiles', [proc_filename, '.mat']);
+    proc_filename = [glider_name, '_L1_', datestr(deployment.start_date, 'yyyy-mm-dd')];
+    processed_data_filename = fullfile(deployment.data_root, 'matfiles', [proc_filename, '.mat']);
     save(processed_data_filename, 'processed_data');
 
-    generateNcProduct(current_deployment, output_dirs, glider_name, 'L1', processed_data);
+    generateNcProduct(deployment, output_dirs, glider_name, 'L1', processed_data);
   catch ME
     disp('could not generate glider processed netcdf file');
     disp(getReport(ME, 'extended'));
-  end;
+  end
 %}
 
-%{
+  
+  %% Process glider trajectory data to vertically gridded data.
+  gridded_data = gridGliderData(processed_data);
+%{    
   % Generate L2 (Gridded) netcdf file and store it
   try
-    generateNcProduct(current_deployment, output_dirs, glider_name, 'L2', gridded_data);
+    generateNcProduct(deployment, output_dirs, glider_name, 'L2', gridded_data);
   catch ME
     disp('could not generate glider gridded netcdf file');
     disp(getReport(ME, 'extended'));
-  end;
+  end
 %}
 
-
+  
   %% Generate deployment figures.
   if isdir(image_dir)
     try
@@ -233,22 +229,22 @@ for deployment_idx = 1:length(active_deployments)
     end;
 %}
     imgs_list = generateScientificFigures(processed_data, gridded_data, ...
-                                          image_dir, [current_deployment.glider_name, '_']);
+                                          image_dir, [deployment.glider_name, '_']);
       % Add URL base path to images
       for idx = 1:length(imgs_list)
         imgs_list(idx).path = fullfile(output_dirs.imageBaseURLPath, ...
-                                       current_deployment.glider_name, ...
-                                       current_deployment.mission_name, ...
+                                       deployment.glider_name, ...
+                                       deployment.mission_name, ...
                                        imgs_list(idx).path);
       end
-      json_name = fullfile( output_dirs.image_base_local_path, ...
-        [current_deployment.glider_name, '.', current_deployment.mission_name, '.images.json']);
+      json_name = fullfile(output_dirs.image_base_local_path, ...
+                           [deployment.glider_name '.' deployment.mission_name '.images.json']);
       writeJSON(imgs_list, json_name);
     catch ME
-      xml_imgs_list = '';
       disp('Error generating scientific figures:');
       disp(getReport(ME, 'extended'));
-    end;
-  end; % if isdir(image_dir)
-end; % for deployment_idx = 1:length(active_deployments)
+    end
+  end
+  
+end
 
