6ce2e049d99b43fc013b5280e4a665b7b969d274
Joan Pau Beltran <joanpau.beltran@socib.cat>
2012-09-17 10:16:16 +0200

    Added NetCDF L1 and L2 product generating function and update L0 one.

--- m/glider_nc_tools/generateOutputNetCDFL0.m
+++ m/glider_nc_tools/generateOutputNetCDFL0.m
@@ -4,28 +4,33 @@ function ncl0 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deploym
 %  NCL0 = GENERATEOUTPUTNETCDFL0(FILENAME, DATA, META, DIMS, ATTS, DEPLOYMENT)
 %  calls WRITENETCDFDATA to generate a NetCDF file named FILENAME from raw 
 %  deployment data in struct DATA, variable metadata in struct META, dimension
-%  names in struct DIMS and global attributes in struct array ATTS.
+%  names in struct DIMS and global attributes in struct array ATTS, and returns
+%  the absolute name of the generated file in string NCL0.
+%  DATA and META should be structs with one field per variable with the variable
+%  data and the variable metadata respectively, as needed by WRITENETCDFDATA.
 %  For raw data, the DIMS argument should be a struct with the following fields 
-%  defining the following dimension names:
+%  defining dimension names:
 %    TIME: name of the time dimension (record dimension).
-%  To allow runtime defined attributes, attributes in ATTS whose name match 
-%  a field name in struct DEPLOYMENT are overwritten with the field value.
+%  ATTS should be a struct array as needed by WRITENETCDFDATA, too. To allow 
+%  runtime defined attributes, attributes in ATTS whose name match a field name 
+%  in struct DEPLOYMENT are overwritten with the field value.
 %  In addition, if the following global attributes are present in struct ATTS, 
 %  they are updated with values computed from deployment data:
 %    DATE_MODIFIED: modification time given by UTC_TIME ('yyyy-mm-ddTHH:MM:SSZ').
-%    GEOSPATIAL_LAT_MAX: maximum latitude value inferred from data (m_gps_lat).
-%    GEOSPATIAL_LAT_MIN: minimum latitude value inferred from data (m_gps_lat).
-%    GEOSPATIAL_LAT_RESOLUTION: mean latitude step inferred from data (m_gps_lat).
-%    GEOSPATIAL_LAT_UNITS: latitude units inferred from metadata (m_gps_lat).
-%    GEOSPATIAL_LON_MAX: maximum longitude value inferred from data (m_gps_lon).
-%    GEOSPATIAL_LON_MIN: minimum longitude value inferred from data (m_gps_lon).
-%    GEOSPATIAL_LON_RESOLUTION: mean longitude step inferred from data (m_gps_lon).
-%    GEOSPATIAL_LON_UNITS: longitude units inferred from metadata (m_gps_lon).
-%    TIME_COVERAGE_END: maximum time value inferred from value (m_present_time).
-%    TIME_COVERAGE_RESOLUTION:  mean time step inferred from data (m_present_time).
-%    TIME_COVERAGE_START: minimum time value inferred from data (m_present_time).
+%    GEOSPATIAL_LAT_MAX: maximum latitude value inferred from data (M_GPS_LAT).
+%    GEOSPATIAL_LAT_MIN: minimum latitude value inferred from data (M_GPS_LAT).
+%    GEOSPATIAL_LAT_RESOLUTION: mean latitude step inferred from data (M_GPS_LAT).
+%    GEOSPATIAL_LAT_UNITS: latitude units inferred from metadata (M_GPS_LAT).
+%    GEOSPATIAL_LON_MAX: maximum longitude value inferred from data (M_GPS_LON).
+%    GEOSPATIAL_LON_MIN: minimum longitude value inferred from data (M_GPS_LON).
+%    GEOSPATIAL_LON_RESOLUTION: mean longitude step inferred from data (M_GPS_LON).
+%    GEOSPATIAL_LON_UNITS: longitude units inferred from metadata (M_GPS_LON).
+%    TIME_COVERAGE_END: maximum time value inferred from value (M_PRESENT_TIME).
+%    TIME_COVERAGE_RESOLUTION:  mean time step inferred from data (M_PRESENT_TIME).
+%    TIME_COVERAGE_START: minimum time value inferred from data (M_PRESENT_TIME).
 %
 %  Notes:
+%    Usually input data is the output of PREPROCESSGLIDERDATA.
 %    Be aware that only variables present in struct META are added to the NetCDF
 %    file. If there is any field in DATA not present in META, it is ommited.
 %
@@ -33,6 +38,7 @@ function ncl0 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deploym
 %    ncl0 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deployment)
 %
 %  See also:
+%    PREPROCESSGLIDERDATA
 %    WRITENETCDFDATA
 %    UTC_TIME
 %
@@ -81,12 +87,12 @@ function ncl0 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deploym
   
   
   %% Select only data properly described by metadata.
-  data_sel = struct();
-  data_field_list = fieldnames(data);
-  for data_field_idx = 1:numel(data_field_list)
-    data_field = data_field_list(data_field_idx);
-    if isfield(meta, data_field);
-      data_sel.(data_field) = data.(data_field);
+  data_selected = struct();
+  data_field_name_list = fieldnames(data);
+  for data_field_name_idx = 1:numel(data_field_name_list)
+    data_field_name = data_field_name_list{data_field_name_idx};
+    if isfield(meta, data_field_name);
+      data_selected.(data_field_name) = data.(data_field_name);
     end
   end  
   
@@ -106,10 +112,11 @@ function ncl0 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deploym
   
   
   %% Generate the file.
-  writeNetCDFData(filename, data, meta, dim_info, global_atts);
+  writeNetCDFData(filename, data_selected, meta, dim_info, global_atts);
   
   
-  %% Return the name of the generated file.
-  ncl0 = filename;
+  %% Return the absolute name of the generated file.
+  [~, ncl0_file_atts, ~] = fileattrib(filename);
+  ncl0 = ncl0_file_atts.Name;
 
 end
---
+++ m/glider_nc_tools/generateOutputNetCDFL1.m
@@ -0,0 +1,122 @@
+function ncl1 = generateOutputNetCDFL1(filename, data, meta, dims, atts, deployment)
+%GENERATEOUTPUTNETCDFL1  Generate NetCDF output for processed glider deployment data.
+%
+%  NCL1 = GENERATEOUTPUTNETCDFL1(FILENAME, DATA, META, DIMS, ATTS, DEPLOYMENT)
+%  calls WRITENETCDFDATA to generate a NetCDF file named FILENAME from processed 
+%  deployment data in struct DATA, variable metadata in struct META, dimension
+%  names in struct DIMS and global attributes in struct array ATTS, and returns
+%  the absolute name of the generated file in string NCL1.
+%  DATA and META should be structs with one field per variable with the variable
+%  data and the variable metadata respectively, as needed by WRITENETCDFDATA.
+%  For precessed data, the DIMS argument should be a struct with the following 
+%  fields defining dimension names:
+%    TIME: name of the time dimension (record dimension).
+%  ATTS should be a struct array as needed by WRITENETCDFDATA, too. To allow 
+%  runtime defined attributes, attributes in ATTS whose name match a field name 
+%  in struct DEPLOYMENT are overwritten with the field value.
+%  In addition, if the following global attributes are present in struct ATTS, 
+%  they are updated with values computed from deployment data:
+%    DATE_MODIFIED: modification time given by UTC_TIME ('yyyy-mm-ddTHH:MM:SSZ').
+%    GEOSPATIAL_LAT_MAX: maximum latitude value inferred from data (LATITUDE).
+%    GEOSPATIAL_LAT_MIN: minimum latitude value inferred from data (LATITUDE).
+%    GEOSPATIAL_LAT_RESOLUTION: mean latitude step inferred from data (LATITUDE).
+%    GEOSPATIAL_LAT_UNITS: latitude units inferred from metadata (LATITUDE).
+%    GEOSPATIAL_LON_MAX: maximum longitude value inferred from data (LONGITUDE).
+%    GEOSPATIAL_LON_MIN: minimum longitude value inferred from data (LONGITUDE).
+%    GEOSPATIAL_LON_RESOLUTION: mean longitude step inferred from data (LONGITUDE).
+%    GEOSPATIAL_LON_UNITS: longitude units inferred from metadata (LONGITUDE).
+%    TIME_COVERAGE_END: maximum time value inferred from value (TIME_NAV).
+%    TIME_COVERAGE_RESOLUTION:  mean time step inferred from data (TIME_NAV).
+%    TIME_COVERAGE_START: minimum time value inferred from data (TIME_NAV).
+%
+%  Notes:
+%    Usually input data is the output of PROCESSGLIDERDATA.
+%    Be aware that only variables present in struct META are added to the NetCDF
+%    file. If there is any field in DATA not present in META, it is ommited.
+%
+%  Examples:
+%    ncl1 = generateOutputNetCDFL0(filename, data, meta, dims, atts, deployment)
+%
+%  See also:
+%    PROCESSGLIDERDATA
+%    WRITENETCDFDATA
+%    UTC_TIME
+%
+%  Author: Joan Pau Beltran
+%  Email: joanpau.beltran@socib.cat
+
+  error(nargchk(6, 6, nargin, 'struct'));
+
+  %% Set dynamic global attributes.
+  dyn_atts = struct();
+  dyn_atts.date_modified = datestr(utc2datenum(utc_time), 'yyyy-mm-ddTHH:MM:SS+00');
+  if isfield(data, 'time_nav')
+    dyn_atts.time_coverage_start = datestr(utc2datenum(min(data.time_nav)), 'yyyy-mm-ddTHH:MM:SS+00');
+    dyn_atts.time_coverage_end = datestr(utc2datenum(max(data.time_nav)), 'yyyy-mm-ddTHH:MM:SS+00');
+    dyn_atts.time_coverage_resolution = sprintf('P%.2fS', min(abs(diff(data.time_nav))));
+  end
+  if isfield(data, 'latitude') && isfield(meta, 'latitude')
+    dyn_atts.geospatial_lat_min = min(data.latitude);
+    dyn_atts.geospatial_lat_max = max(data.latitude);
+    dyn_atts.geospatial_lat_resolution = min(abs(diff(data.latitude)));
+    [lat_has_units, lat_units_idx] = ismember('units', {meta.latitude.attributes.name});
+    if lat_has_units
+      dyn_atts.geospatial_lat_units = meta.latitude.attributes(lat_units_idx).value;
+    end
+  end
+  if isfield(data, 'longitude') && isfield(meta, 'longitude')
+    dyn_atts.geospatial_lon_min = min(data.longitude);
+    dyn_atts.geospatial_lon_max = max(data.longitude);
+    dyn_atts.geospatial_lon_resolution = min(abs(diff(data.longitude)));
+    [lon_has_units, lon_units_idx] = ismember('units', {meta.longitude.attributes.name});
+    if lon_has_units
+      dyn_atts.geospatial_lon_units = meta.longitude.attributes(lon_units_idx).value;
+    end
+  end
+ 
+
+  %% Overwrite default attributes with deployment fields or dynamic values.
+  global_atts = atts;
+  for atts_idx = 1:numel(atts)
+    if isfield(deployment, atts(atts_idx).name)
+      global_atts(atts_idx).value = deployment.(atts(atts_idx).name);
+    elseif isfield(dyn_atts, atts(atts_idx).name)
+      global_atts(atts_idx).value = dyn_atts.(atts(atts_idx).name);
+    end
+  end
+  
+  
+    %% Select only data properly described by metadata.
+  data_selected = struct();
+  data_field_name_list = fieldnames(data);
+  for data_field_name_idx = 1:numel(data_field_name_list)
+    data_field_name = data_field_name_list{data_field_name_idx};
+    if isfield(meta, data_field_name);
+      data_selected.(data_field_name) = data.(data_field_name);
+    end
+  end  
+    
+  
+  %% Set dimension lengths.
+  dim_info = struct(dims.time, {0});
+  
+  
+  %% Create base directory of target file if needed.
+  [file_dir, ~, ~] = fileparts(filename);
+  if ~exist(file_dir,'dir')
+    [success, error_msg] = mkdir(file_dir);
+    if ~success
+      error('glider_toolbox:netcdf_tools:NetCDFDirectoryError', error_msg);
+    end
+  end
+  
+  
+  %% Generate the file.
+  writeNetCDFData(filename, data_selected, meta, dim_info, global_atts);
+  
+  
+  %% Return the absolute name of the generated file.
+  [~, ncl1_file_atts, ~] = fileattrib(filename);
+  ncl1 = ncl1_file_atts.Name;
+
+end
