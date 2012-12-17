ef9b901c653438d0e3c24ff9994489bc01083e73
Joan Pau Beltran <joanpau.beltran@socib.cat>
2012-12-17 18:17:53 +0100

    Switched to common optional argument format handling (dual mode) and improved documentation comments.

--- m/processing_tools/preprocessGliderData.m
+++ m/processing_tools/preprocessGliderData.m
@@ -1,25 +1,29 @@
-function data_pre = preprocessGliderData(data_raw, options)
+function data_pre = preprocessGliderData(data_raw, varargin)
 %PREPROCESSGLIDERDATA  Preprocess glider deployment data.
 %
-%  [DATA_PRE, META_PRE] = PREPROCESSGLIDERDATA(DATA_RAW, OPTIONS) preprocesses
-%  glider deployment data in struct DATA_RAW according to option struct OPTIONS,
-%  returning the preprocessed data in struct DATA_PRE. It performs the following
+%  Syntax:
+%    [DATA_PRE, META_PRE] = PREPROCESSGLIDERDATA(DATA_RAW)
+%    [DATA_PRE, META_PRE] = PREPROCESSGLIDERDATA(DATA_RAW, OPTIONS)
+%    [DATA_PRE, META_PRE] = PREPROCESSGLIDERDATA(DATA_RAW, OPT1, VAL1, ...)
+%
+%  [DATA_PRE, META_PRE] = PREPROCESSGLIDERDATA(DATA_RAW, ...) preprocesses
+%  glider deployment data according to given options, performing the following 
 %  actions:
 %    - Convert NMEA latitude/longitude format to decimal degrees.
 %
-%  DATA_RAW should be a struct with vector fields containing time series of
-%  sensor data and named after the corresponding sensor.
+%  DATA_RAW should be a struct in the format returned by DBACAT or DBAMERGE,
+%  where each field is a time sequence from the sensor with the same name.
 %
-%  OPTIONS should be a struct with the following preprocessing option fields:
-%    NMEA_COORD_SENSORS: string cell array with the list of latitude/longitude 
-%      to be converted from NMEA degrees to decimal degrees.
+%  Options may be given either as key-value pairs OPT1, VAL1 ... or in a struct
+%  OPTIONS with field names as option keys and field values as option values.
+%  Recognized options are:
+%    NMEA_CONVERSION_SENSOR_LIST: string cell array with the latitude or 
+%      longitude sensor sequences to be converted from NMEA to decimal degrees.
+%      Default value is empty list, any conversion applied.
 %
 %  DATA_PRE is a copy of DATA_RAW with the following conversions applied:
-%    - Each serie in NMEA_COORD_SENSORS option is converted from NMEA degrees to
-%      decimal degrees using the function NMEA2DEG.
-%
-%  All fields in struct OPTIONS are optional. If any field is missing the
-%  conversion is not applied.
+%    - Each serie in NMEA_CONVERSION_SENSOR_LIST option is converted from NMEA 
+%      degrees to decimal degrees using the function NMEA2DEG.
 %
 %  Examples:
 %    data_pre = preprocessGliderData(data_raw, options)
@@ -30,18 +34,45 @@ function data_pre = preprocessGliderData(data_raw, options)
 %  Author: Joan Pau Beltran
 %  Email: joanpau.beltran@socib.cat
 
-  error(nargchk(2, 2, nargin));
+  error(nargchk(1, 3, nargin, 'struct'));
   
-  % Initialize output data.
-  data_pre = data_raw;
-  
-  % NMEA to decimal degree conversion.
-  nmea_sensors = {};
-  if isfield(options, 'nmea_coord_sensors')
-    nmea_sensors = options.nmea_coord_sensors;
+  %% Set preprocessing options.
+  % Set default options values.
+  options = struct();
+  options.nmea_conversion_sensor_list = {};  
+  % Parse option key-value pairs in any accepted call signature.
+  if isscalar(varargin) && isstruct(varargin{1})
+    % Options passed as a single option struct argument.
+    option_key_list = fieldnames(varargin{1});
+    option_val_list = struct2cell(varargin{1});
+  elseif mod(numel(varargin), 2) == 0
+    % Options passed as key-value argument pairs.
+    option_key_list = varargin(1:2:end);
+    option_val_list = varargin(2:2:end);
+  else
+    error('glider_toolbox:processGliderData:InvalidOption', ...
+          'Invalid optional arguments (neither key-value pairs nor struct).');
   end
-  for sensor_idx = 1:numel(nmea_sensors)
-    sensor_name = nmea_sensors{sensor_idx};
+  % Overwrite default options with values given in extra arguments.
+  for opt_idx = 1:numel(option_key_list)
+    opt = lower(option_key_list{opt_idx});
+    val = option_val_list{opt_idx};
+    if isfield(options, opt)
+      options.(opt) = val;
+    else
+      error('glider_toolbox:processGliderData:InvalidOption', ...
+            'Invalid option: %s.', opt);
+    end
+  end
+
+
+  %% Initialize output data.
+  data_pre = data_raw;
+
+
+  %% NMEA to decimal degree conversion.
+  for sensor_idx = 1:numel(options.nmea_conversion_sensor_list)
+    sensor_name = options.nmea_conversion_sensor_list{sensor_idx};
     if isfield(data_pre, sensor_name)
       data_pre.(sensor_name) = nmea2deg(data_pre.(sensor_name));
     end
