b1896957f25e19290b7ece5eca5c670c2647123d
Joan Pau Beltran <joanpau.beltran@socib.cat>
2013-07-16 15:09:49 +0200

    Fixed error selecting legend position in profile statistics plot function.

--- m/plotting_tools/plotProfileStatistics.m
+++ m/plotting_tools/plotProfileStatistics.m
@@ -199,7 +199,7 @@ function [hfig, haxs, hlgs, hlns] = plotProfileStatistics(varargin)
         'XData', [(mmean(:) - mstd(:)); nan; (mmean(:) + mstd(:))]);
     reverse_x = strcmp(get(haxs(s), 'XDir'), 'reverse');
     reverse_y = strcmp(get(haxs(s), 'YDir'), 'reverse');
-    increasing = [1 0] * nancov(mmean(:), ydata(:), 1) * [0 1]';
+    increasing = [1 0] * nancov(mmean(:), ydata(:), 1) * [0 1]' > 0;
     legend_location_list = {'NorthWest' 'NorthEast'; 'SouthWest' 'SouthEast'};
     legend_location = ...
       legend_location_list{1 + reverse_y, ...
