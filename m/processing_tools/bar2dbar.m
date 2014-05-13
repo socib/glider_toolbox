8f15ab8603ea3eec0164e631982bccb772fd2999
Joan Pau Beltran <joanpau.beltran@socib.cat>
2014-05-13 17:42:50 +0200

    Add pressure conversion function.
    Pressure is used in decibars but some sensors repors it in bars.
    Even though the conversion is simply a factor of 10,
    it is convenient to provide a named function `bar2dbar`
    to perform it when needed.

---
+++ m/processing_tools/bar2dbar.m
@@ -0,0 +1,42 @@
+function dbar = bar2dbar(bar)
+%BAR2DBAR  Convert pressure from bars to dbars.
+%
+%  Syntax:
+%    DBAR = BAR2DBAR(BAR)
+%
+%  DBAR = BAR2DBAR(BAR) converts pressure readings in array BAR from bars to
+%  decibars by multiplying by 10.
+%
+%  Notes:
+%    This is simply a convenience function to call the conversion with an
+%    explicit name.
+%
+%  Examples:
+%    dbar = bar2dbar(bar)
+%
+%  See also:
+%
+%  Author: Joan Pau Beltran
+%  Email: joanpau.beltran@socib.cat
+
+%  Copyright (C) 2014
+%  ICTS SOCIB - Servei d'observacio i prediccio costaner de les Illes Balears.
+%
+%  This program is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  error(nargchk(nargin, 1, 1, 'struct'));
+  
+  dbar = bar * 10;
+
+end
